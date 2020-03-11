e32bec1d6ecd7f7f2d163f5157075ded3385bc46
Ivan Steuerberg <irodriguezsteuerberg@danielcastelao.org>
2020-03-11 13:42:12 +0100

    Contar hasta 20 y fin de la cita

--- src/ejramas/EjRamas.java
+++ src/ejramas/EjRamas.java
@@ -8,9 +8,15 @@ public class EjRamas {
 
     public static void main(String[] args) {
 
+        //Hola mundo
         System.out.println("Hola mundo");
-
-
+        //Contar hasta 20
+        for (int numero = 0; numero < 20; numero++) {
+            System.out.println(numero);
+        }
+        //Mostrar fin de la cita
+        System.out.println("Fin de la cita");
+         
     }
     
 }
