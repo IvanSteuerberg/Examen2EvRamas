31bcac823df111701db22c13cc49c9409dcbb1ef
Ivan Steuerberg <irodriguezsteuerberg@danielcastelao.org>
2020-03-11 13:33:20 +0100

    Inicio, hola mundo

---
+++ src/ejramas/EjRamas.java
@@ -0,0 +1,16 @@
+package ejramas;
+
+/**
+ *
+ * @author irodriguezsteuerberg
+ */
+public class EjRamas {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hola mundo");
+
+
+    }
+    
+}
