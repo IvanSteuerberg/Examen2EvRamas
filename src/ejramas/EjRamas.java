f3d4703686597c8f1d46718215f15cad93d552d7
Ivan Steuerberg <irodriguezsteuerberg@danielcastelao.org>
2020-03-11 13:39:20 +0100

    Contar hasta 50 y adios

--- src/ejramas/EjRamas.java
+++ src/ejramas/EjRamas.java
@@ -7,10 +7,14 @@ package ejramas;
 public class EjRamas {
 
     public static void main(String[] args) {
-
+        //Mostrar hola mundo por pantalla
         System.out.println("Hola mundo");
-
-
+        //Contar hasta 50
+        for (int numero = 0; numero < 50; numero++) {
+            System.out.println(numero);    
+        }
+        //Mostrar adios por pantalla
+        System.out.println("Adios");
     }
     
 }
